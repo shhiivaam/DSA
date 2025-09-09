029eea7c0a6343b7200a13c9ee5b195305ec75d6
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-09-09 19:59:59 +0530

    Time: 55 ms (18.86%), Space: 7.8 MB (98%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s = "";
+        for (int i = 1; i <= n; i++) {
+            s += (i + '0');
+        }
+        sort(s.begin(), s.end());
+        k--;
+        while (k--) {
+            next_permutation(s.begin(), s.end());
+        }
+        return s;
+    }
+};
