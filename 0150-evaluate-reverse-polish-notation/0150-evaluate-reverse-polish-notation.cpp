5e16c9c21981b1ead0f6d981665b8277b9dfdc56
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-09-16 12:34:59 +0530

    Time: 0 ms (100%), Space: 17.2 MB (34.96%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int evalRPN(vector<string>& arr) {
+        stack<int> st;
+        for (int i = 0; i < arr.size(); i++) {
+            if (isdigit(arr[i][0]) || (arr[i].size() > 1 && arr[i][0] == '-')) {
+                int num = stoi(arr[i]);
+                st.push(num);
+            } else {
+                if (st.size() >= 2) {
+                    int b = st.top(); st.pop();
+                    int a = st.top(); st.pop();
+
+                    if (arr[i] == "*") {
+                        st.push(a * b);
+                    } else if (arr[i] == "/") {
+                        st.push(a / b);
+                    } else if (arr[i] == "^") {
+                        st.push(pow(a, b)); // exponentiation
+                    } else if (arr[i] == "+") {
+                        st.push(a + b);
+                    } else if (arr[i] == "-") {
+                        st.push(a - b);
+                    }
+                }
+            }
+        }
+        return st.top();
+    }
+};
