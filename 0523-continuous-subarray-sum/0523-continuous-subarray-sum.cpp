deb41a84c4954051d0e3202bbcf45c91296decfc
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-09-24 10:05:53 +0530

    Time: 74 ms (76.57%), Space: 146.3 MB (54.5%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-           int prefix = 0;
+    int prefix = 0;
     unordered_map<int, int> prefixToIndex{{0, -1}};
     for (int i = 0; i < nums.size(); ++i) {
       prefix += nums[i];
@@ -15,7 +15,6 @@ public:
         prefixToIndex[prefix] = i;
       }
     }
-
     return false;
     }
 };
