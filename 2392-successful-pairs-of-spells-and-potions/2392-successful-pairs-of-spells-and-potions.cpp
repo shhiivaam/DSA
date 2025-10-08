dcd183fa3a7de184a8ca18b3d6a66d58d1c1b2aa
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-10-08 21:13:37 +0530

    Time: 38 ms (86.98%), Space: 128.4 MB (25.75%) - LeetHub

---
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,24 @@
+class Solution {
+ public:
+  vector<int> successfulPairs(vector<int>& spells, vector<int>& potions,
+                              long long success) {
+    vector<int> ans;
+    ranges::sort(potions);
+    for (const int spell : spells)
+      ans.push_back(potions.size() - firstIndexSuccess(spell, potions, success));
+    return ans;
+  }
+ private:
+  int firstIndexSuccess(int spell, const vector<int>& potions, long success) {
+    int l = 0;
+    int r = potions.size();
+    while (l < r) {
+      const int m = (l + r) / 2;
+      if (static_cast<long>(spell) * potions[m] >= success)
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  }
+};
