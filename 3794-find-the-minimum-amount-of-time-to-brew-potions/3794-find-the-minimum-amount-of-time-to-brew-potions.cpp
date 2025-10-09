80ca4a36242a7062f37080c9fa75ea0a5b2fc2c5
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-10-09 21:17:40 +0530

    Time: 91 ms (98.99%), Space: 45.5 MB (43.43%) - LeetHub

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/3794-find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     long long minTime(vector<int>& skill, vector<int>& mana) {
-    long sumSkill = accumulate(skill.begin(), skill.end(), 0L);
-    long prevWizardDone = sumSkill * mana[0];
-    for (int j = 1; j < mana.size(); ++j) {
-      long prevPotionDone = prevWizardDone;
-      for (int i = skill.size() - 2; i >= 0; --i) {
-        prevPotionDone -= static_cast<long>(skill[i + 1]) * mana[j - 1];
-        prevWizardDone =
-            max(prevPotionDone,
-                prevWizardDone - static_cast<long>(skill[i]) * mana[j]);
-      }
-      prevWizardDone += sumSkill * mana[j];
-    }
-
-    return prevWizardDone;
+        int n = skill.size();
+        int m = mana.size();
+        if (m == 0) {
+            return 0;
+        }
+        vector<long long> p(n);
+        p[0] = skill[0];
+        for (int i = 1; i < n; ++i) {
+            p[i] = p[i - 1] + skill[i];
+        }
+        long long startTime = 0;
+        for (int j = 1; j < m; ++j) {
+            long long m_prev = mana[j - 1];
+            long long m_curr = mana[j];
+            
+            long long delta = p[0] * m_prev;
+            for (int i = 1; i < n; ++i) {
+                delta = max(delta, p[i] * m_prev - p[i - 1] * m_curr);
+            }
+            startTime += delta;
+        }
+        return startTime + p[n - 1] * (long long)mana.back();
     }
 };
