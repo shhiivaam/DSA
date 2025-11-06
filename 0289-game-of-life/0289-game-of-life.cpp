4620705c7b983c3c60037147c81d915a558a0607
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-11-06 13:20:20 +0530

    Time: 0 ms (100%), Space: 9.5 MB (86.47%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        int m = board.size(), n = board[0].size();
+        vector<int> dirs = {-1, 0, 1};
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int live = 0;
+                for (int dx : dirs) {
+                    for (int dy : dirs) {
+                        if (dx == 0 && dy == 0) continue;
+                        int x = i + dx, y = j + dy;
+                        if (x >= 0 && x < m && y >= 0 && y < n && abs(board[x][y]) == 1)
+                            live++;
+                    }
+                }
+                if (board[i][j] == 1 && (live < 2 || live > 3))
+                    board[i][j] = -1; 
+                if (board[i][j] == 0 && live == 3)
+                    board[i][j] = 2;
+            }
+        }
+
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                board[i][j] = board[i][j] > 0 ? 1 : 0;
+    }
+};
