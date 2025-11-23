a9c0126a8a0fb6d48e5cbda7726887c15bf5158a
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-11-23 09:56:01 +0530

    Time: 3 ms (86.49%), Space: 29 MB (78.06%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -1,56 +1,13 @@
 class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
-        int total = 0;
-        vector<int> rem1;
-        vector<int> rem2;
-        for (int x : nums) {
-            total = total + x;
-            if (x % 3 == 1) {
-                rem1.push_back(x);
-            }
-            else if (x % 3 == 2) {
-                rem2.push_back(x);
-            }
+        int dp[3] = {0, -1000000000, -1000000000};
+        for (int n : nums) {
+            int a = dp[0], b = dp[1], c = dp[2];
+            if (a != -1000000000) dp[(a + n) % 3] = max(dp[(a + n) % 3], a + n);
+            if (b != -1000000000) dp[(b + n) % 3] = max(dp[(b + n) % 3], b + n);
+            if (c != -1000000000) dp[(c + n) % 3] = max(dp[(c + n) % 3], c + n);
         }
-        sort(rem1.begin(), rem1.end());
-        sort(rem2.begin(), rem2.end());
-        if (total % 3 == 0) {
-            return total;
-        }
-        int answer = 0;
-        if (total % 3 == 1) {
-            int option1 = INT_MAX;
-            int option2 = INT_MAX;
-            if (rem1.size() >= 1) {
-                option1 = rem1[0];
-            }
-            if (rem2.size() >= 2) {
-                option2 = rem2[0] + rem2[1];
-            }
-            int remove_value = min(option1, option2);
-            if (remove_value == INT_MAX) {
-                return 0;
-            }
-            answer = total - remove_value;
-        }
-        else { 
-            int option1 = INT_MAX;
-            int option2 = INT_MAX;
-
-            if (rem2.size() >= 1) {
-                option1 = rem2[0];
-            }
-            if (rem1.size() >= 2) {
-                option2 = rem1[0] + rem1[1];
-            }
-            int remove_value = min(option1, option2);
-
-            if (remove_value == INT_MAX) {
-                return 0;
-            }
-            answer = total - remove_value;
-        }
-        return answer;
+        return dp[0];
     }
 };
