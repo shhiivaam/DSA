56737d59c8142f382dbdaf471af0d0cbe2809539
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-11-25 11:19:58 +0530

    Time: 0 ms (100%), Space: 8.4 MB (16.74%) - LeetHub

--- 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.cpp
@@ -1,13 +1,43 @@
 class Solution {
+    long long power(long long base, long long exp, long long mod) {
+        long long res = 1;
+        base %= mod;
+        while (exp > 0) {
+            if (exp % 2 == 1) res = (res * base) % mod;
+            base = (base * base) % mod;
+            exp /= 2;
+        }
+        return res;
+    }
+    long long getPhi(long long n) {
+        long long result = n;
+        for (long long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0)
+                    n /= i;
+                result -= result / i;
+            }
+        }
+        if (n > 1)
+            result -= result / n;
+        return result;
+    }
 public:
     int smallestRepunitDivByK(int k) {
-    if (k % 2 == 0 || k % 5 == 0) return -1;  
-
-    int num = 0;
-    for (int i = 1; i <= 100000; i++) {
-        num = (num * 10 + 1) % k;
-        if (num == 0) return i;
+        if (k % 2 == 0 || k % 5 == 0) return -1;
+        long long m = 9LL * k;
+        long long phi = getPhi(m);
+        vector<long long> divisors;
+        for (long long i = 1; i * i <= phi; i++) {
+            if (phi % i == 0) {
+                divisors.push_back(i);
+                if (i * i != phi) divisors.push_back(phi / i);
+            }
+        }
+        sort(divisors.begin(), divisors.end());
+        for (long long d : divisors) {
+            if (power(10, d, m) == 1) return d;
+        }
+        return -1;
     }
-    return -1;
-}
 };
