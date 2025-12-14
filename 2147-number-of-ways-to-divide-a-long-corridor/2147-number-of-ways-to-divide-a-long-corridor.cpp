8a30ff2c5060301a5c238f879b910ed613d87d75
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2025-12-14 19:58:20 +0530

    Time: 42 ms (24%), Space: 58.5 MB (51.5%) - LeetHub

---
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numberOfWays(string corridor) {
+        vector<int> seat_indices;
+        for (int i = 0; i < corridor.length(); i++) {
+            if (corridor[i] == 'S') {
+                seat_indices.push_back(i);
+            }
+        }
+        if (seat_indices.size() == 0 || seat_indices.size() % 2 != 0) {
+            return 0;
+        }
+        
+        long long ans = 1;
+        long long MOD = 1e9 + 7;
+        for (int i = 2; i < seat_indices.size(); i += 2) {
+            int prev_seat_index = seat_indices[i - 1];
+            int curr_seat_index = seat_indices[i];
+            long long ways_for_this_gap = curr_seat_index - prev_seat_index;
+            ans = (ans * ways_for_this_gap) % MOD;
+        }
+        return (int)ans;
+    }
+};
