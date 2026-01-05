65af3f431a67a2fd4bc824b5fcd1c9ae1ee830d4
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-01-05 20:44:25 +0530

    Time: 0 ms (100%), Space: 39 MB (88.21%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -2,29 +2,20 @@ class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long max_sum = 0;
-        int n = matrix.size();
-        int m = matrix[0].size();
-        vector<int> neg;
-        int mini = 1e8;
-        int zero = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] < 0) {
-                    int ele = matrix[i][j];
-                    ele *= -1;
-                    neg.push_back(ele);
-                }
-                if(matrix[i][j] > 0) max_sum += matrix[i][j];
+        int neg = 0;  int mini = 1e8;  int zero = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[i].size(); j++) {
+                if (matrix[i][j] < 0)
+                    neg++;
+                max_sum += abs(matrix[i][j]);
                 mini = min(abs(matrix[i][j]), mini);
                 if (matrix[i][j] == 0)
                     zero++;
             }
         }
-        int len = neg.size();
-        if (len % 2 == 0 || zero > 0) {
-            max_sum += accumulate(neg.begin(), neg.end(), 0LL);
+        if (neg % 2 == 0 || zero > 0) {
+            return max_sum;
         } else {
-            max_sum += accumulate(neg.begin(), neg.end(), 0LL);
             max_sum -= 2LL * mini;
         }
         return max_sum;
