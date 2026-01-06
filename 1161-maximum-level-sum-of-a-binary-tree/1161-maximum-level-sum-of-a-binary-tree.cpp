3321ec5445053b6f6bf3d1f200b31d774f7f1ad7
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-01-06 18:26:14 +0530

    Time: 3 ms (68.92%), Space: 107.1 MB (94.32%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.cpp
@@ -3,13 +3,10 @@ public:
     void _solve(TreeNode* root, int level, vector<long long>& levelSum) {
         if (root == NULL)
             return;
-
         if (level >= levelSum.size()) {
             levelSum.push_back(0);
         }
-
         levelSum[level] += root->val;
-
         _solve(root->left, level + 1, levelSum);
         _solve(root->right, level + 1, levelSum);
     }
@@ -17,9 +14,9 @@ public:
     int maxLevelSum(TreeNode* root) {
         vector<long long> levelSum;
         _solve(root, 0, levelSum);
-
+        
         auto it = max_element(levelSum.begin(), levelSum.end());
         int level = it - levelSum.begin() + 1;
-        return level;
+       return level;
     }
 };
