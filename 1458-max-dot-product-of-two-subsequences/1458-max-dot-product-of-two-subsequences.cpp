af14cd3fa0918404e70d6d9cbe12ca1be79de685
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-01-08 12:17:33 +0530

    Time: 14 ms (78%), Space: 17.8 MB (54.8%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-int _solve(vector<int>& a, vector<int>& b, int n, int m,
-vector<vector<int>>&dp) {
-    if (n < 0 || m < 0) return INT_MIN / 2;
-    if(dp[n][m] != -1) return dp[n][m];
+    int _solve(vector<int>& a, vector<int>& b, int n, int m,
+               vector<vector<int>>& dp) {
+        if (n < 0 || m < 0)
+            return INT_MIN / 2;
+        if (dp[n][m] != -1)
+            return dp[n][m];
 
-    int take = a[n] * b[m];
-    take = max(take, a[n] * b[m] + _solve(a, b, n-1, m-1,dp));
+        int take = a[n] * b[m];
+        take = max(take, a[n] * b[m] + _solve(a, b, n - 1, m - 1, dp));
 
-    int skipA = _solve(a, b, n-1, m,dp);
-    int skipB = _solve(a, b, n, m-1,dp);
+        int skipA = _solve(a, b, n - 1, m, dp);
+        int skipB = _solve(a, b, n, m - 1, dp);
 
-    return dp[n][m] = max({take, skipA, skipB});
-}
-    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-       int n =nums1.size();
-       int m = nums2.size(); 
-       vector<vector<int>>dp(n,vector<int>(m,-1));
-       int ans = _solve(nums1,nums2,n-1,m-1,dp); 
-       return ans;
+        return dp[n][m] = max({take, skipA, skipB});
+    }
+    int maxDotProduct(vector<int>& a, vector<int>& b) {
+    int n = a.size();
+    int m = b.size();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MIN / 2));
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+
+            int take = a[i] * b[j];
+            take = max(take, a[i] * b[j] + dp[i + 1][j + 1]);
+
+            int skipA = dp[i + 1][j];
+            int skipB = dp[i][j + 1];
+
+            dp[i][j] = max({take, skipA, skipB});
+        }
+    }
+    return dp[0][0];
     }
 };
