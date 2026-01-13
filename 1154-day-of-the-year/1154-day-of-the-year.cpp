8bb343ff76a35d74123661a698c464097595cacb
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-01-13 20:20:28 +0530

    Time: 3 ms (81.77%), Space: 12.4 MB (21.95%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.cpp
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int dayOfYear(string date) {
-        struct tm t = {0};
-        t.tm_year = stoi(date.substr(0, 4)) - 1900; 
-        t.tm_mon  = stoi(date.substr(5, 2)) - 1; 
-        t.tm_mday = stoi(date.substr(8, 2)); 
-        mktime(&t);
-        return t.tm_yday + 1;
+        int year = stoi(date.substr(0, 4));
+        int month = stoi(date.substr(5, 2));
+        int day = stoi(date.substr(8, 2));
+        int totalDays = 31 * (month - 1) + day;
+        if (month > 2) {
+            totalDays -= (4 * month + 23) / 10;
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                totalDays += 1;
+            }
+        }
+        return totalDays;
     }
 };
