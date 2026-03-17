af8b0db5226147f25f5c8a067ee350cf04354331
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-03-17 19:00:14 +0530

    Time: 64 ms (5.15%), Space: 179.9 MB (38.16%) - LeetHub

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
-
 long long dp[100001];
-
 long long solve(int idx, vector<int>& costs){
-
     if(idx == 0) return 0;
 
     if(dp[idx] != -1) return dp[idx];
-
     long long ans = LLONG_MAX;
-
     if(idx >= 1){
         ans = min(ans, solve(idx-1, costs) + costs[idx-1] + 1);
     }
-
     if(idx >= 2){
         ans = min(ans, solve(idx-2, costs) + costs[idx-1] + 4);
     }
-
     if(idx >= 3){
         ans = min(ans, solve(idx-3, costs) + costs[idx-1] + 9);
     }
-
     return dp[idx] = ans;
 }
 
 int climbStairs(int n, vector<int>& costs) {
-
     memset(dp, -1, sizeof(dp));
-
     return solve(n, costs);
 }
 
