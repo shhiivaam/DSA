b1781292a85915b2e69e6b071ef7397e36566ea9
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-04-06 22:42:30 +0530

    Time: 22 ms (68.8%), Space: 37.9 MB (56.53%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        constexpr int kDirs[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int ans = 0;
+        int d = 0; 
+        int x = 0; 
+        int y = 0; 
+        unordered_set<pair<int, int>, PairHash> obstaclesSet;
+
+        for (const vector<int>& obstacle : obstacles) {
+            const int x = obstacle[0];
+            const int y = obstacle[1];
+            obstaclesSet.insert({x, y});
+        }
+
+        for (const int command : commands) {
+            if (command == -1) {
+                d = (d + 1) % 4;
+            } else if (command == -2) {
+                d = (d + 3) % 4;
+            } else {
+                for (int step = 0; step < command; ++step) {
+                    if (obstaclesSet.contains(
+                            {x + kDirs[d][0], y + kDirs[d][1]}))
+                        break;
+                    x += kDirs[d][0];
+                    y += kDirs[d][1];
+                }
+            }
+            ans = max(ans, x * x + y * y);
+        }
+
+        return ans;
+    }
+
+private:
+    struct PairHash {
+        size_t operator()(const pair<int, int>& p) const {
+            return p.first ^ p.second;
+        }
+    };
+};
