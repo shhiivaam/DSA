415ce860f386d56f436a5d49e0e558499baaec04
Shivam kumar <163742486+shhiivaam@users.noreply.github.com>
2026-05-13 20:52:20 +0530

    Time: 11 ms (52.21%), Space: 93.4 MB (83.19%) - LeetHub

---
+++ 1674-minimum-moves-to-make-array-complementary/1674-minimum-moves-to-make-array-complementary.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  int minMoves(vector<int>& nums, int limit) {
+    const int n = nums.size();
+    int ans = n;
+    vector<int> delta(limit * 2 + 2);
+
+    for (int i = 0; i < n / 2; ++i) {
+      const int a = nums[i];
+      const int b = nums[n - 1 - i];
+      --delta[min(a, b) + 1];
+      --delta[a + b];
+      ++delta[a + b + 1];
+      ++delta[max(a, b) + limit + 1];
+    }
+    for (int i = 2, moves = n; i <= limit * 2; ++i) {
+      moves += delta[i];
+      ans = min(ans, moves);
+    }
+
+    return ans;
+  }
+};
